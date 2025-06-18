206742b2cca80b1a3aeedf9493de07ed4906fcc8
datpthe171311 <phamtand75@gmail.com>
2025-06-18 12:04:53 +0700

    them login regiter, forget

---
+++ Data/DTO/Response/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TourManagement_BE.Data.DTO.Response
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+    }
+}
+
---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TourManagement_BE.Data.DTO.Request;
+using TourManagement_BE.Data.Models;
+
+namespace TourManagement_BE.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterRequest, User>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.Avatar));
+        }
+    }
+}
