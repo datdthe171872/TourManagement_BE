e3212d56d557b6e071d343124fc864410fa0cea0
huytrandoquang <huytdqhe173448@fpt.edu.vn>
2025-06-20 01:01:23 +0700

    Manage Profile

---
+++ Data/DTO/Request/ProfileRequest/UpdateProfileRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace TourManagement_BE.Data.DTO.Request.ProfileRequest
+{
+    public class UpdateProfileRequest
+    {
+        public int? UserId { get; set; } 
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public IFormFile? AvatarFile { get; set; }
+    }
+
+}
---
+++ Data/DTO/Response/ProfileResponse/UserProfileResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace TourManagement_BE.Data.DTO.Response.ProfileResponse
+{
+    public class UserProfileResponse
+    {
+        public int? UserId { get; set; }
+        public string? UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Avatar { get; set; }
+        public string RoleName { get; set; } = null!;
+        public bool? IsActive { get; set; }
+
+        public string? CompanyName { get; set; }
+        public string? Description { get; set; }
+
+        public int? HomeTourGuideId { get; set; }
+    }
+
+}
