be5164509da99f6dbb0fe4b1180daab23c671786
datpthe171311 <phamtand75@gmail.com>
2025-07-28 12:18:20 +0700

    xong

--- Data/DTO/Request/CreateExtraChargeRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace TourManagement_BE.Data.DTO.Request
-{
-    public class CreateExtraChargeRequest
-    {
-        public string Name { get; set; }
-        public string? Description { get; set; }
-        public decimal Amount { get; set; }
-    }
-} 
